00344864b2e569b0872f725d306134e47e08115a
Palash Chauhan <palchauh@adobe.com>
2019-02-20 13:16:17 +0530

    added timer to quiz

--- 1-Quiz_Game/main.go
+++ 1-Quiz_Game/main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"encoding/csv"
 	"flag"
+	"fmt"
+	"os"
 	"strings"
+	"time"
 )
 
 //helps if input comes from something other than csv files
 type problem struct {
 	question string
-	answer string
+	answer   string
 }
 
 func main() {
 
 	//get csv file through command line arguments
 	var csvFileName = flag.String("csv", "problems.csv", "csv filename with problem set with format: question,answer")
+	timeLimit := flag.Int("limit", 13, "time limit (sec) for the quiz")
 	flag.Parse()
 
 	//open and read file
-	file, err := os.Open(*csvFileName)  //flag package returns pointer
+	file, err := os.Open(*csvFileName) //flag package returns pointer
 	if err != nil {
 		exit(fmt.Sprintf("Falied to open the csv file: %s", *csvFileName))
 	}
@@ -33,34 +35,47 @@ func main() {
 		exit("failed to parse csv file")
 	}
 	problems := parseLines(lines)
-	
+
+	//timer
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	//show problems to user
 	correct_answers := 0
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d: %s = ", i+1, problem.question)
-		// read answer
-		var ans string
-		fmt.Scanf("%s\n", &ans)
-		if ans == problem.answer {
-			correct_answers++
+		answerChannel := make(chan string)
+		go func() {
+			// read answer
+			//use goroutine to handle blocking of scanf
+			var ans string
+			fmt.Scanf("%s\n", &ans)
+			answerChannel <- ans
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou got %d/%d correct answers!", correct_answers, len(problems))
+			return
+		case ans := <-answerChannel:
+			if ans == problem.answer {
+				correct_answers++
+			}
 		}
 	}
 	fmt.Printf("You got %d/%d correct answers!", correct_answers, len(problems))
-	
 }
 
 func parseLines(lines [][]string) []problem {
-	 probs := make([]problem, len(lines))
-	 for i, line := range lines {
-	 	probs[i] = problem{
-	 				question:line[0], 
-	 				answer:strings.TrimSpace(line[1]),
-	 				}
-	 }
-	 return probs
+	probs := make([]problem, len(lines))
+	for i, line := range lines {
+		probs[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return probs
 }
 
-func exit(msg string){
+func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
 }

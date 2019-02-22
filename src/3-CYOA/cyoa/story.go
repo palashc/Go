85c8a13c48bfaa6c66533349803290bd1c3168c5
Palash Chauhan <palchauh@adobe.com>
2019-02-22 14:36:59 +0530

    story transitions

--- src/3-CYOA/cyoa/story.go
+++ src/3-CYOA/cyoa/story.go
@@ -3,7 +3,9 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -39,10 +41,20 @@ type handler struct {
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.New("").Parse(defaultHTTPTemplate))
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+	path = path[1:]
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found!", http.StatusNotFound)
 }
 
 var defaultHTTPTemplate = `
